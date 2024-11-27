2b8935618c73620f036d6ddeb8dbea411783272d
Khushdeep Sharma <124013688+khushsharma509@users.noreply.github.com>
2024-11-27 09:33:06 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "https://real-time-chat-app-1-y1e0.onrender.com/api" : "/api",
   withCredentials: true,
 });
